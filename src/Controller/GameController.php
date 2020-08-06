3aaae78ba9e4d7a6c23c57c8f700cd827434c2bb
KenSerikawa <kenserikawalucio@gmail.com>
2020-08-06 14:30:33 +0200

    Include GameSetter for retrieving test data to view

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -6,13 +6,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\GameSetter;
 
 class GameController extends AbstractController
 {
-    public function index()
+    public function index(Request $request, GameSetter $setter)
     {
+        $result = $setter->__invoke();
         return $this->render('game/index.html.twig', [
-            
+            'results' => $result['results']
         ]);
     }
 }
