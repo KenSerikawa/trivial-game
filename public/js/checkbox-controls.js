df07e1c7eddabdb6a3f81fe8a64a5df7fe484697
KenSerikawa <kenserikawalucio@gmail.com>
2020-08-10 20:49:25 +0200

    Remove unnecessary bool var

--- public/js/checkbox-controls.js
+++ public/js/checkbox-controls.js
@@ -2,15 +2,12 @@ function selectOne(id, element) {
     let optionsList = $('#' + id)[0]
     let options = optionsList.children
     if(hasAnyChecked(options, element)) {
-
     }
 }
 
 function hasAnyChecked(options, elementChecked) {   
-    $isChecked = false
-    console.log(elementChecked)
     for (let i = 0; i < options.length; i++) {
-        const element = options[i].childNodes[1]
+        const element = options[i].childNodes[3]
         if(element.checked && element !== elementChecked){
             element.checked = false
         }
