4fc1e34153f6791540287c35cba015e42d2ad993
KenSerikawa <kenserikawalucio@gmail.com>
2020-08-15 17:14:52 +0200

    Add background and font color on selected option list

--- public/js/checkbox-controls.js
+++ public/js/checkbox-controls.js
@@ -2,6 +2,20 @@ function selectOne(id, element) {
     let optionsList = $('#' + id)[0]
     let options = optionsList.children
     hasAnyChecked(options, element)
+    addBackground(element.parentElement)
+}
+
+function addBackground(option) {
+    option.classList.add('bg-white')
+    option.classList.add('text-dark')
+    option.classList.add('rounded')
+}
+
+function removeBackground(option) {
+    console.log(option)
+    option.classList.remove('bg-white')
+    option.classList.remove('text-dark')
+    option.classList.remove('rounded')
 }
 
 function hasAnyChecked(options, elementChecked) {   
@@ -9,6 +23,7 @@ function hasAnyChecked(options, elementChecked) {
         const element = options[i].childNodes[3]
         if(element.checked && element !== elementChecked){
             element.checked = false
+            removeBackground(element.parentElement)
         }
     }
 }
