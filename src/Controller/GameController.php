9ced0a625b3baae352523616c2f8c6cbc4b67664
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-12 16:59:59 +0200

    Add service properly generated on new method

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -8,7 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Service\GameSetter;
-use App\Entity\Game;
 use App\Form\GameType;
 
 class GameController extends AbstractController
@@ -16,11 +15,23 @@ class GameController extends AbstractController
     public function new(Request $request)
     {
         $form = $this->createForm(GameType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $game = $form->getData();
+            $setter = new GameSetter($game->getQuestionNumber(), $game->getCategory(), $game->getDifficulty(), $game->getType());
+            $result = $setter->__invoke();
+
+            return $this->render('game/index.html.twig', [
+                'results' => $result['results']
+            ]);
+        }
 
         return $this->render('game/new.html.twig', [
             'form' => $form->createView(),
         ]);
     }
+
     public function index(Request $request, GameSetter $setter)
     {
         $result = $setter->__invoke();
