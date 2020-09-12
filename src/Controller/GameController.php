cf09658117fce06a202fa3c265d138b4c78b7649
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-12 02:03:06 +0200

    Add new method for setting trivial parameters (new game basically)

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -8,9 +8,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Service\GameSetter;
+use App\Entity\Game;
+use App\Form\GameType;
 
 class GameController extends AbstractController
 {
+    public function new(Request $request)
+    {
+        $form = $this->createForm(GameType::class);
+
+        return $this->render('game/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
     public function index(Request $request, GameSetter $setter)
     {
         $result = $setter->__invoke();
