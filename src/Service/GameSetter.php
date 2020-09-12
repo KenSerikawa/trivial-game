62c743e77e31fb6d1e3077e5b7d1d52cd48dd34b
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-12 02:03:48 +0200

    Add new prroperty: type

--- src/Service/GameSetter.php
+++ src/Service/GameSetter.php
@@ -9,17 +9,20 @@ final class GameSetter
     private $questions;
     private $category;
     private $difficulty;
+    private $type;
 
-    public function __construct(string $questions='10', string $category='', string $difficulty='')
+    public function __construct(string $questions='10', string $category='', string $difficulty='', string $type='')
     {
         $this->questions = $questions;
         $this->category = $category;
         $this->difficulty = $difficulty;
+        $this->type = $type;
     }
 
     public function __invoke()
     {
         $url = 'https://opentdb.com/api.php?amount=' . $this->questions;
+
         return json_decode(file_get_contents($url), true);
     }
 }
