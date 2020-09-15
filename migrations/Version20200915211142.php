94644e0e25bb8ff86a171615a69eba0d61222364
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-15 23:19:53 +0200

    Add first migration

---
+++ migrations/Version20200915211142.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200915211142 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE game (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, difficulty VARCHAR(255) NOT NULL, question_number VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE records (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(20) NOT NULL, right_answers INT NOT NULL, total_questions INT NOT NULL, date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE game');
+        $this->addSql('DROP TABLE record');
+    }
+}
