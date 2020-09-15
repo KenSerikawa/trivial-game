55409a43ce6f1b3f0415d3aaeaee86225bedc658
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-16 00:02:44 +0200

    Add new record method and route

--- src/Controller/RecordController.php
+++ src/Controller/RecordController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Record;
 use App\Repository\RecordRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class RecordController extends AbstractController
 {
@@ -20,5 +22,19 @@ class RecordController extends AbstractController
         return $this->render('record/index.html.twig', [
             'results' => $results
         ]);
+    }
+    public function new(Request $request)
+    {
+        $form_data = $request->request->all();
+        $record = new Record();
+        $record->setUsername($form_data['nickname']);
+        $record->setRightAnswers($form_data['right_answers']);
+        $record->setTotalQuestions($form_data['total_questions']);
+        $record->setDate();
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($record);
+        $em->flush();
+
+        return $this->redirectToRoute('records');
     }
 }
